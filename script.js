2935596d7a8464321077b886b9c600dce1ace57a
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2021-03-30 11:28:35 -0500

    all features except overheal hearts work

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const hitButton = controlsContainer.querySelector("#hit-button");
 const hitDamageInput = controlsContainer.querySelector("#hit-damage-input");
 const healButton = controlsContainer.querySelector("#heal-button");
 const healInput = controlsContainer.querySelector("#heal-amount-input");
+const addHeart = controlsContainer.querySelector("#add-heart-container-button");
 let health = 35;
-let maxHealth = 40;
+let maxHealth = 44;
+let overHealth = 0;
 
 function randint(lo, hi) {
   return Math.floor(Math.random() * (hi - lo) + lo);
@@ -31,8 +33,16 @@ hitButton.addEventListener("click", function () {
 });
 
 healButton.addEventListener("click", function () {
-  let bonus = Number(healInput.value);
-  health += bonus;
+  let bonus = Number(healInput.value) * 4;
+  if (health <= maxHealth) {
+    health = Math.min(maxHealth, health+bonus);
+  }
   updateHeartsDisplay();
 });
 
+addHeart.addEventListener("click", function () {
+  let newHeart = heartsContainer.querySelector(".heart").cloneNode(true);
+  heartsContainer.appendChild(newHeart);
+  maxHealth += 4;
+  updateHeartsDisplay();
+});
